80894f5712dc3db6ad66eff89661c1b0f8b10405
Daichi Yanagi <michaelyanagi@gmail.com>
2019-01-06 00:49:07 +0900

    3. Conditional Rendering

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ var app = new Vue({
     el: '#app',
     data: {
         product: 'Books',
-        image: './assets/green-socks.jpg'
+        image: './assets/green-socks.jpg',
+        inStock: true,
+        inventory: 10
     }
 });
