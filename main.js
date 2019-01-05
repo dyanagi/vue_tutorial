e696083910ecbbb16c611b68184ab9d839fe88cf
Daichi Yanagi <michaelyanagi@gmail.com>
2019-01-06 01:01:22 +0900

    4. List Rendering

--- main.js
+++ main.js
@@ -4,6 +4,18 @@ var app = new Vue({
         product: 'Books',
         image: './assets/green-socks.jpg',
         inStock: true,
-        inventory: 10
+        inventory: 10,
+        details: ["80% cotton", "20% polyester", "Gender-neutral"],
+        variants: [
+            {
+                variantId: 2234,
+                variantColour: "green"
+            },
+            {
+                variantId: 2235,
+                variantColour: "blue"
+            }
+        ],
+        sizes: ["L", "M", "S"]
     }
 });
