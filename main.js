f25546f5e41927a9829e2f93246e854ea875afeb
Daichi Yanagi <michaelyanagi@gmail.com>
2019-01-06 07:41:38 +0900

    7. Computed Properties

--- main.js
+++ main.js
@@ -1,21 +1,22 @@
 var app = new Vue({
     el: '#app',
     data: {
+        brand: 'Vue Mastery',
         product: 'Books',
-        image: './assets/green-socks.jpg',
-        inStock: true,
-        inventory: 10,
+        selectedVariant: 0,
         details: ["80% cotton", "20% polyester", "Gender-neutral"],
         variants: [
             {
                 variantId: 2234,
                 variantColour: "green",
-                variantImage: './assets/green-socks.jpg'
+                variantImage: './assets/green-socks.jpg',
+                variantQuantity: 10
             },
             {
                 variantId: 2235,
                 variantColour: "blue",
-                variantImage: './assets/blue-socks.jpg'
+                variantImage: './assets/blue-socks.jpg',
+                variantQuantity: 0
             }
         ],
         sizes: ["L", "M", "S"],
@@ -29,8 +30,21 @@ var app = new Vue({
         removeFromCart: function () {
             this.cart = 0
         },
-        updateProduct: function (variantImage) {
-            this.image = variantImage
+        updateProduct: function (index) {
+            this.selectedVariant = index;
+            console.log(index) // how to log
+        }
+    },
+    // Computed properties are cashed and more efficient than methods
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product
+        },
+        image() {
+            return this.variants[this.selectedVariant].variantImage
+        },
+        inStock() {
+            return this.variants[this.selectedVariant].variantQuantity // 0 is "falsey"
         }
     }
 });
