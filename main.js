60512636ce96eb36fa352de1ab669273606f3fe4
Daichi Yanagi <michaelyanagi@gmail.com>
2019-01-06 01:38:48 +0900

    5 Event Handling

--- main.js
+++ main.js
@@ -9,13 +9,28 @@ var app = new Vue({
         variants: [
             {
                 variantId: 2234,
-                variantColour: "green"
+                variantColour: "green",
+                variantImage: './assets/green-socks.jpg'
             },
             {
                 variantId: 2235,
-                variantColour: "blue"
+                variantColour: "blue",
+                variantImage: './assets/blue-socks.jpg'
             }
         ],
-        sizes: ["L", "M", "S"]
+        sizes: ["L", "M", "S"],
+        cart: 0
+    },
+    methods: {
+        // ES6 shorthand: 'addToCart()'. Not all browsers may support this feature.
+        addToCart: function () {
+            this.cart += 1
+        },
+        removeFromCart: function () {
+            this.cart = 0
+        },
+        updateProduct: function (variantImage) {
+            this.image = variantImage
+        }
     }
 });
